97c3ddd96ccd03314f3cfb3eaee225564fbc9bae
寒溪 <taihe.hth@alibaba-inc.com>
2022-09-23 19:04:51 +0800

    init

---
+++ starter-spring-boot-log-access/src/main/java/com/github/huotaihe/log/access/spring/AccessLogAutoConfiguration.java
@@ -0,0 +1,11 @@
+package com.github.huotaihe.log.access.spring;
+
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @author 寒溪
+ * @date 2022/09/23
+ */
+@Configuration
+public class AccessLogAutoConfiguration {
+}
---
+++ starter-spring-boot-log-access/src/test/java/com/github/huotaihe/log/access/spring/StarterSpringBootLogAccessApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.huotaihe.log.access.spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StarterSpringBootLogAccessApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
